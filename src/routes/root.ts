449a8da947f34decf80ceb97f1382f7bd16a7a54
Simone <simone>
2023-12-09 12:48:06 +0100

    add websocket

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -1,8 +1,14 @@
 import { FastifyPluginAsync } from 'fastify'
 
 const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.get('/', async function (request, reply) {
-    return { root: true }
+  fastify.get('/', { websocket: true }, (connection /* SocketStream */, req /* FastifyRequest */) => {
+    connection.socket.on('message', message => {
+      // message.toString() === 'hi from client'
+      connection.socket.send(JSON.stringify({
+        method: 'message1',
+        payload: 'hi from server'
+      }))
+    })
   })
 }
 
